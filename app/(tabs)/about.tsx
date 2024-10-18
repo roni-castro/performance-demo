331ac2d7f14d8773f1648077d264e77e346b6980
Roni Castro <roni.c.castro@gmail.com>
2024-10-18 17:47:34 -0300

    feat: update about screen text

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function About() {
   return (
     <View style={styles.container}>
-        <Text style={styles.text}>About screen</Text>
+        <Text style={styles.text}>About</Text>
     </View>
   );
 }
