896317e3396572792aabfda520ead7326923bda6
Roni Castro <roni.c.castro@gmail.com>
2024-11-06 15:06:25 -0300

    chore: configure react compiler

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ["expo", "prettier"],
-  plugins: ["prettier", "react-native"],
+  plugins: ["prettier", "react-native", "eslint-plugin-react-compiler"],
   rules: {
     "prettier/prettier": "error",
     "react-native/no-unused-styles": "error",
+    "react-compiler/react-compiler": "error",
   },
   ignorePatterns: ["!src/**/*"],
   overrides: [
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          "react-compiler": {
+            sources: (filename) => {
+              // Match file names to include in the React Compiler.
+              return ["src/components", "src/features", "src/hooks"].some(
+                (dir) => filename.includes(dir),
+              );
+            },
+          },
+        },
+      ],
+    ],
   };
 };
